cc2d1c212edce3e6d7c258c2886b702f73aed8c3
mahabur17 <m.rahman15424@gmail.com>
2025-10-23 20:14:35 +0600

    all style has done

---
+++ src/script.js
@@ -0,0 +1,9 @@
+// let addbtn=document.querySelector('#show-add-btn')
+// let divone=document.querySelector('.div1')
+// let divtwo=document.querySelector('.div2')
+
+// addbtn.addEventListener('click',()=>{
+//     //    divone.classList.add('hidden');
+//     //    divtwo.classList.remove('hidden')
+
+// })

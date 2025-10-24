18bdf5a74d9a9fcab4769ae92467e984d5b01ae8
mahabur17 <m.rahman15424@gmail.com>
2025-10-24 12:31:38 +0600

    add and withdraw money

--- src/script.js
+++ src/script.js
@@ -17,9 +17,18 @@ let divfour=document.querySelector('.div4')
 let addinput=document.querySelector('#add-amount')
 let withdrawinput=document.querySelector('#withdraw-amount')
 
+//access error
+let showerror=document.querySelectorAll('#add-error')
+
+//current balance
+let currentbalance=document.querySelector('#currentbalance')
+
+
+//btn functions
 addbtn.addEventListener('click',()=>{
        divone.classList.add('hidden');
          divtwo.classList.remove('hidden');
+        
         
 
 })
@@ -35,9 +44,64 @@ transactionbtn.addEventListener('click',()=>{
 
 for(x of dashboardbtn){
     x.addEventListener('click',()=>{
-   divone.classList.remove('hidden')
+   
+    divone.classList.remove('hidden')
       divthree.classList.add('hidden');
         divtwo.classList.add('hidden');
           divfour.classList.add('hidden');
     })
 }
+
+// confirm add button
+confirmaddbtn.addEventListener('click',()=>{
+  
+  let toadd=Number(addinput.value);
+  let current=Number(currentbalance.textContent);
+
+   if(toadd==""){
+          showerror[0].classList.remove('hidden');
+       
+         }
+
+    if(toadd!==""){
+       showerror[0].classList.add('hidden');  
+      let newbalance=current+toadd;
+
+         currentbalance.textContent =newbalance.toFixed(2);
+         addinput.value=""
+             
+           divone.classList.remove('hidden');
+         divtwo.classList.add('hidden');
+      
+    }
+})
+
+// 
+confirmwithdrawbtn.addEventListener('click',()=>{
+  
+  let toremove=Number(withdrawinput.value);
+  let current=Number(currentbalance.textContent);
+
+   if(toremove==""){
+          showerror[1].classList.remove('hidden');
+       
+         }
+
+    if(toremove > current){
+      alert('Insufficient Fund')
+      withdrawinput.value=""
+    }
+
+    if(toremove!=="" && toremove < current ){
+       showerror[1].classList.add('hidden');  
+      let newbalance=current-toremove;
+
+         currentbalance.textContent =newbalance.toFixed(2);
+         withdrawinput.value=""
+             
+           divone.classList.remove('hidden');
+         divthree.classList.add('hidden');
+      
+    }
+    
+})

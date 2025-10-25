cfb0b748d1a73e3c5c3a76061dddfde5dcb84646
mahabur17 <m.rahman15424@gmail.com>
2025-10-25 09:41:22 +0600

    all done

--- src/script.js
+++ src/script.js
@@ -12,6 +12,8 @@ let divone=document.querySelector('.div1')
 let divtwo=document.querySelector('.div2')
 let divthree=document.querySelector('.div3')
 let divfour=document.querySelector('.div4')
+let maindiv=document.querySelector('.main')
+let parentdiv=document.querySelector('.parentdiv')
  
 //access of inputs
 let addinput=document.querySelector('#add-amount')
@@ -22,6 +24,8 @@ let showerror=document.querySelectorAll('#add-error')
 
 //current balance
 let currentbalance=document.querySelector('#currentbalance')
+
+
 
 
 //btn functions
@@ -58,31 +62,54 @@ confirmaddbtn.addEventListener('click',()=>{
   let toadd=Number(addinput.value);
   let current=Number(currentbalance.textContent);
 
-   if(toadd==""){
+   if(addinput.value==""){
           showerror[0].classList.remove('hidden');
        
          }
 
-    if(toadd!==""){
+    if(addinput.value!==""){
        showerror[0].classList.add('hidden');  
       let newbalance=current+toadd;
 
-         currentbalance.textContent =newbalance.toFixed(2);
+         currentbalance.textContent =newbalance;
          addinput.value=""
              
            divone.classList.remove('hidden');
          divtwo.classList.add('hidden');
+
+         let copydiv=maindiv.cloneNode(true);
+         let time=copydiv.querySelector('.time')
+         let transitiontype=copydiv.querySelector('.type')
+         let balance=copydiv.querySelector('.balance')
+         let new_balance=copydiv.querySelector('.newbalance')
+
+
+        let now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let ampm = hours >= 12 ? 'PM' : 'AM';
+let day = now.getDate();
+let month = now.toLocaleString('default', { month: 'short' });
+
+ let timeString = `${month} ${day}, ${hours}:${minutes} ${ampm}`;
+
+  time.textContent=timeString;
+
+         transitiontype.textContent="Add"
+         balance.textContent=`+$${toadd}`;
+         new_balance.textContent=`$${newbalance}`;
       
+         parentdiv.appendChild(copydiv)
     }
 })
 
-// 
+// confirm withdraw btn
 confirmwithdrawbtn.addEventListener('click',()=>{
   
   let toremove=Number(withdrawinput.value);
   let current=Number(currentbalance.textContent);
 
-   if(toremove==""){
+   if(withdrawinput.value==""){
           showerror[1].classList.remove('hidden');
        
          }
@@ -92,16 +119,61 @@ confirmwithdrawbtn.addEventListener('click',()=>{
       withdrawinput.value=""
     }
 
-    if(toremove!=="" && toremove < current ){
+    if(withdrawinput.value!=="" && toremove <= current ){
        showerror[1].classList.add('hidden');  
       let newbalance=current-toremove;
 
-         currentbalance.textContent =newbalance.toFixed(2);
+         currentbalance.textContent =newbalance;
          withdrawinput.value=""
              
            divone.classList.remove('hidden');
          divthree.classList.add('hidden');
+
+          let copydiv=maindiv.cloneNode(true);
+         let time=copydiv.querySelector('.time')
+         let transitiontype=copydiv.querySelector('.type')
+         let balance=copydiv.querySelector('.balance')
+         let new_balance=copydiv.querySelector('.newbalance')
+
+
+        let now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let ampm = hours >= 12 ? 'PM' : 'AM';
+let day = now.getDate();
+let month = now.toLocaleString('default', { month: 'short' });
+
+ let timeString = `${month} ${day}, ${hours}:${minutes} ${ampm}`;
+
+  time.textContent=timeString;
+
+         transitiontype.textContent="withdraw"
+         transitiontype.style.color="green"
+         balance.textContent=`-$${toremove}`;
+         new_balance.textContent=`$${newbalance}`;
+      
+         parentdiv.appendChild(copydiv)
       
     }
     
+
+
 })
+
+
+// time add 
+let maintime=document.querySelector('#maintime')
+function updateTime() {
+          let now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let ampm = hours >= 12 ? 'PM' : 'AM';
+let day = now.getDate();
+let month = now.toLocaleString('default', { month: 'short' });
+
+ maintime.textContent = `${month} ${day}, ${hours}:${minutes} ${ampm}`;
+
+}
+updateTime()
+
+setInterval(updateTime,1000)
